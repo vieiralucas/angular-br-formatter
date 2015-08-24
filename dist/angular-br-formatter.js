384c3862bd1a86839815fcc88711dfeec8bb104c
Lucas Vieira <vieiralucas4@gmail.com>
2015-08-24 01:16:34 -0300

    Release v0.0.2

--- dist/angular-br-formatter.js
+++ dist/angular-br-formatter.js
@@ -1,30 +1,38 @@
-angular.module('br-formatter', []);
+(function() {
+    'use strict';
 
-angular
-    .module('br-formatter')
-    .filter('brPhoneFormatter', brPhoneFormatter);
+    angular.module('br-formatter', []);
+}());
 
-function brPhoneFormatter() {
-    return function(value) {
-        if (!value || typeof value !== 'string') {
-            return '';
-        }
+(function() {
+    'use strict';
 
-        value = value.replace(/[^0-9.]/g, ""); // remove everything but digits
+    angular
+        .module('br-formatter')
+        .filter('brPhoneFormatter', brPhoneFormatter);
 
-        var length = value.length;
-
-        if (length < 6) {
-            return value;
-        } else {
-            if (length < 7) {
-                return '(' + value.substring(0, 2) + ') ' + value.substring(2, length);
-            } else if (length === 9) {
-                return '(' + value.substring(0, 2) + ') ' + value.substring(2, 5) + '-' + value.substring(5, length);
-            } else {
-                return '(' + value.substring(0, 2) + ') ' + value.substring(2, 6) + '-' + value.substring(6, length);
+    function brPhoneFormatter() {
+        return function(value) {
+            if (!value || typeof value !== 'string') {
+                return '';
             }
-        }
+
+            value = value.replace(/[^0-9.]/g, ''); // remove everything but digits
+
+            var length = value.length;
+
+            if (length < 6) {
+                return value;
+            } else {
+                if (length < 7) {
+                    return '(' + value.substring(0, 2) + ') ' + value.substring(2, length);
+                } else if (length === 9) {
+                    return '(' + value.substring(0, 2) + ') ' + value.substring(2, 5) + '-' + value.substring(5, length);
+                } else {
+                    return '(' + value.substring(0, 2) + ') ' + value.substring(2, 6) + '-' + value.substring(6, length);
+                }
+            }
+        };
     }
-}
+}());
 
