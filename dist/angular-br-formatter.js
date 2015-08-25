6abf8438a98a5089c30d1a2c7222af2d310d7282
Lucas Vieira <vieiralucas4@gmail.com>
2015-08-25 05:50:24 -0300

    Release v0.2.0

--- dist/angular-br-formatter.js
+++ dist/angular-br-formatter.js
@@ -9,6 +9,41 @@
 
     angular
         .module('br-formatter')
+        .directive('brPhone', brPhone);
+
+    brPhone.$inject = ['$filter'];
+    function brPhone($filter) {
+        var brPhoneFormatter = $filter('brPhoneFormatter');
+
+        return {
+            restrict: 'A',
+            require: '^ngModel',
+            scope: {
+                ngModel: '='
+            },
+            link: linkFn
+        };
+
+        function linkFn(scope) {
+            if (scope.ngModel) {
+                scope.ngModel = brPhoneFormatter(scope.ngModel);
+            }
+
+            scope.$watch('ngModel', function(newVal, oldVal) {
+                if (oldVal || newVal !== oldVal) {
+                    scope.ngModel = brPhoneFormatter(scope.ngModel);
+                }
+            });
+        }
+    }
+}());
+
+
+(function() {
+    'use strict';
+
+    angular
+        .module('br-formatter')
         .filter('brPhoneFormatter', brPhoneFormatter);
 
     function brPhoneFormatter() {
